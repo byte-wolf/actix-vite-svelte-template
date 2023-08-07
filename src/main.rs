5cc3150447d4eb3ac2a29f2766bb383223d74133
byte-wolf <s.wolffhardt@protonmail.com>
2023-08-08 00:37:29 +0200

    fixed favicon

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,22 @@ async fn index() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
+        //print!("{}", std::env::current_dir().expect("Fisch").to_str().expect("Fisch"));
         App::new()
             .service(
                 web::scope("/api")
                     .service(index)
             )
             .service(
-                actix_files::Files::new("/", String::from("/client/dist/"))
+                actix_files::Files::new("/", "./client/build")
+                    //.show_files_listing()
+                    .index_file("index.html")
+                    .default_handler(
+                        actix_files::NamedFile::open(
+                            ["./client/build".to_string() , "index.html".to_string()].join("/"),
+                        )
+                        .expect("index file should exist"),
+                    ),
             )
     })
     .bind(("localhost", 8080))?
